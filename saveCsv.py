b45f0a539b539f79725ede55caa9de636a297c1e
gimdaehyeon <kdhdh560@naver.com>
2021-12-15 13:01:45 +0900

    부류ㅣㄹ요한 코드 제거

--- saveCsv.py
+++ saveCsv.py
@@ -13,30 +13,4 @@ def df_save_csv(data, filename):
         df_csv = data.to_csv(filename, encoding='utf-8')
         print(f"{filename} 저장")
 
-        return filename
-
-    # if data.shape[1] == 2:
-    #     if os.path.isfile(filename):
-    #         print("url 기존 파일")
-    #         with open(filename, 'a') as url_csv:
-    #             data.to_csv(url_csv, header=False)
-    #         url_csv.close()
-    #         print("url 추가 끝")
-    #
-    #     else:
-    #         df_csv = data.to_csv(filename, encoding='utf-8')
-    #         print(f"{filename} 저장")
-    #         return filename
-    #
-    # else:
-    #     if os.path.isfile(filename):
-    #         print("기존 파일 감지")
-    #         with open(filename, 'a') as w_csv:
-    #             print("기존 파일에 추가")
-    #             data.to_csv(w_csv, header=False)
-    #             w_csv.close()
-    #             return print("추가 끝")
-    #     else:
-    #         df_csv = data.to_csv(filename, encoding='utf-8')
-    #         print(f"{filename} 저장")
-    #     return filename
+    return filename
